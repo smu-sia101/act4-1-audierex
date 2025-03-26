4a1cbdfd59522dc8249e3338cf134efbf242d15a
AudieRexValencia <AudieRexValencia@SMU-0134>
2025-03-26 17:05:50 +0800

    init commit

--- PetNameGenerated/PetNameGenerated/Controllers/PetGenerator.cs
+++ PetNameGenerated/PetNameGenerated/Controllers/PetGenerator.cs
@@ -1,96 +1,73 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Net;
 
 namespace PetNameGenerated.Controllers
 {
     [ApiController]
-    [Route(" [controller]")]
+    [Route("[controller]")]
     public class PetGenerator : ControllerBase
     {
+      
         private string[] dog = new string[] { "Cooper", "Canolord", "Doggy", "Ryan", "Rex" };
         private string[] cat = new string[] { "Kiffy", "Mittens", "PussyCat", "Simba", "Tiger" };
         private string[] bird = new string[] { "Birdy", "Junior", "Chirpy", "Raven", "Sunny" };
 
-        [HttpGet("/generate")]
+        [HttpPost("generate")]
         public IActionResult GeneratePetName([FromBody] PetRequest request)
         {
-
+           
             if (string.IsNullOrEmpty(request.AnimalType))
             {
-                return BadRequest(new { error = "animalType is required" });
-
+                return BadRequest(new { error = "The 'animalType' field is required." });
             }
-            string[] selectedNames = request.AnimalType.ToLower() switch
-            {"dog" => dog, "cat" => cat, "bird" => bird,_ => null};
-
-            if (selectedNames == null)
+            string[] PetNames;
+            switch (request.AnimalType.ToLower())
             {
-                return BadRequest(new { error = "Invalid animalType. Use 'dog', 'cat', or 'bird'." });
+                case "dog":
+                    PetNames = dog;
+                    break;
+                case "cat":
+                    PetNames = cat;
+                    break;
+                case "bird":
+                    PetNames = bird;
+                    break;
+                default:
+                    return BadRequest(new { error = "Invalid animal type. Allowed values: dog, cat, bird." });
             }
 
-           
+          
+            if (request.TwoPart.HasValue && !(request.TwoPart.Value is bool))
+            {
+                return BadRequest(new { error = "The 'twoPart' field must be a boolean (true or false)." });
+            }
+
+         
             Random rnd = new Random();
             string petAnimalName;
 
             if (request.TwoPart == true)
             {
-                
-                string name1 = selectedNames[rnd.Next(selectedNames.Length)];
-                string name2 = selectedNames[rnd.Next(selectedNames.Length)];
+               
+                string name1 = PetNames[rnd.Next(PetNames.Length)];
+                string name2 = PetNames[rnd.Next(PetNames.Length)];
                 petAnimalName = $"{name1} {name2}";
             }
             else
             {
-                
-                petAnimalName = selectedNames[rnd.Next(selectedNames.Length)];
+                petAnimalName = PetNames[rnd.Next(PetNames.Length)];
             }
 
-            return Ok(new { petName = petAnimalName });
+           
+            return Ok(new { name = petAnimalName });
         }
     }
 
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public class PetRequest
     {
         public string AnimalType { get; set; }
         public bool? TwoPart { get; set; } 
     }
 }
-    
